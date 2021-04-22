d27d664b7d5fe2964a96c4fdb23db0624732e916
daveyrojo <d.vincenteldridge@gmail.com>
2021-04-22 01:26:17 -0400

    still has not deployed correcty

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { ModalCard } from "./components/ModalCard";
 
 function App() {
   return (
-    <Router basename="/david-portfolio">
+    <Router basename="/david-eldridge">
       <NavBar />
       <Switch>
         <Route exact path="/">
