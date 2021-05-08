a5ca3519af9040ea8acce2c1e816899efa60846e
daveyrojo <d.vincenteldridge@gmail.com>
2021-05-08 17:04:30 -0400

    added contact info to header so email is always present

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -8,10 +8,24 @@ class NavBar extends Component {
     return (
       <Navbar expand="lg" variant="dark" bg="dark" sticky="top">
         <Container fluid>
-          <Navbar.Brand><Link style={{color: 'white'}} className="brand" to="/">David Eldridge.</Link></Navbar.Brand>
-          <Link style={{color: 'primary'}} to="/">Home.</Link>
-          <Link style={{color: 'green'}} to="/aboutme">About Me.</Link>
-          <Link style={{color: 'primary'}} to="/projects">Projects.</Link>
+          <Navbar.Brand>
+            <Link style={{ color: "white" }} className="brand" to="/">
+              David Eldridge.
+            </Link>
+          </Navbar.Brand>
+          <Link style={{ color: "primary" }} to="/">
+            Home.
+          </Link>
+          <Link style={{ color: "green" }} to="/aboutme">
+            About Me.
+          </Link>
+          <Link style={{ color: "primary" }} to="/projects">
+            Projects.
+          </Link>
+          <div className="column" style={{ color: "white" }}>
+            <p>Email: d.vincenteldridge@gmail.com</p>
+            <p>Phone: 347.469.2996</p>
+          </div>
         </Container>
       </Navbar>
     );
