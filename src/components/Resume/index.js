d5ea5e07b710f8500bec25db769654aacf137239
daveyrojo <d.vincenteldridge@gmail.com>
2021-05-08 17:36:33 -0400

    made resume component

---
+++ src/components/Resume/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+function Resume () {
+  return (
+    <Card style={{ textAlign: "center", marginTop: "2.5em" }} fluid>
+      <Card.Title style={{ textAlign: "center" }}>Resume:</Card.Title>
+      <a
+        style={{ color: "gray", fontSize: 10 }}
+        type="application/pdf"
+        rel="noreferrer"
+        target="_blank"
+        href="https://pdfhost.io/v/4ExQmXzuh_resumeRDpdf.pdf"
+      >
+        <Card.Img
+          className="resumeLink"
+          style={{ width: 240, height: 300 }}
+          src="https://i.imgur.com/66uKXXn.jpg"
+          fluid
+        />
+        Click to Download
+      </a>
+    </Card>
+  );
+};
+
+export default Resume;
