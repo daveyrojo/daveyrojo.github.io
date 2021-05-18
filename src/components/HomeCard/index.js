128b5f8712fc0e3d12baf4687f66bb81d4a0df26
david eldridge <d.vincenteldridge@gmail.com>
2021-05-18 18:34:26 -0400

    fixed grammar in about me

--- src/components/HomeCard/index.js
+++ src/components/HomeCard/index.js
@@ -16,23 +16,21 @@ function HomeCard() {
         fluid
       />
       <p>
-        Full-Stack web developer with a strong background in business and
-        operations management that allows for innovative problem solving in a
-        way that makes sense for clients and the bottom line. Recently earned a
-        certificate in full stack development from Rutgers University, with
-        skills in JavaScript, CSS, databases, React.js and much more - all of
-        which can be applied for a responsive web app build. My goal is to
-        conduct myself in a professional manner that flies in the face of the
-        status quo for programmers and engage with clients to such a degree that
-        the sales process continues throughout the project to ensure clients are
-        retained easily. I have built out projects that vary from front and
-        backend api databases sites to cryptocurrency market tracking, all of
-        which I took the lead for development ideas on and helped manage from
-        start to completion. My management successes coupled with my desire to
-        learn and improve means that what I bring to the table is an opportunity
-        to succeed in the short term and then thrive in the long term, you will
-        know that you have a capable leader in your ranks who is ready to lead
-        whenever needed.
+        Full-Stack Web Developer with a strong background in business and
+        operations management. Work process allows for innovative problem
+        solving in a way that makes sense for clients. Recently earned a
+        certificate in Full Stack Development from Rutgers University,
+        developing skills in JavaScript, CSS, databases, React.js and much more
+        - all of which can be applied for a responsive web application build. My
+        goal is to conduct myself in a professional manner with programmers and
+        engage with clients throughout the project to establish and build
+        trusting relationships. I have built out projects that vary from front
+        and backend api databases sites to cryptocurrency market tracking, all
+        of which I took the lead for development ideas on and helped manage from
+        start to completion. My management successes, coupled with my desire to
+        learn and improve, brings an opportunity to succeed in the short term
+        and then thrive in the long term. You will know that you have a capable
+        leader in your ranks who is ready to lead whenever needed.
       </p>
     </div>
   );
